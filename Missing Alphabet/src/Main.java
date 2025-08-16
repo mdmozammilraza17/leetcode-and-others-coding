b5b5411f524124629bbbe9dd88de2962e21e3d9a
mdmozammilraza17 <mdmozammilraza06@gmail.com>
2025-08-17 00:28:54 +0530

    Missing Alphabets

--- Missing Alphabet/src/Main.java
+++ Missing Alphabet/src/Main.java
@@ -1,5 +1,47 @@
+// KPMG coding interview
+// String str = "This is a girl"
+// Find the missing alphabet in this
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution
+{
+    public String missingAlphabet (String str)
+    {
+        StringBuilder result = new StringBuilder();
+        String lowercaseconversion = str.toLowerCase().replaceAll(" ", "");
+        char[] chars = lowercaseconversion.toCharArray();
+        Arrays.sort(chars);
+
+        for (int c = 'a'; c <= 'z'; c++)
+        {
+            boolean found = false;
+            // Search in sorted array
+            for (char ch: chars)
+            {
+                if (ch == c)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                result.append((char)c).append(" ");
+            }
+        }
+        return result.toString();
+
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String str = "This is a girl";
+        Solution solution = new Solution();
+        String result = solution.missingAlphabet(str);
+        System.out.println("Missing Alphabets: "+result);
     }
 }
