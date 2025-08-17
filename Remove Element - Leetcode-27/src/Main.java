d719da4d1dd1453c2184c39f4cd8869b1c454e1d
mdmozammilraza17 <mdmozammilraza06@gmail.com>
2025-08-17 20:26:31 +0530

    Remove element

--- Remove Element - Leetcode-27/src/Main.java
+++ Remove Element - Leetcode-27/src/Main.java
@@ -1,5 +1,30 @@
+class Solution
+{
+    public int removeElements (int nums[], int val)
+    {
+        int j = 0;
+        for (int i=0; i<nums.length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int nums [] = {3, 2, 2, 3};
+        int val = 3;
+        Solution solution = new Solution();
+        int result = solution.removeElements(nums, val);
+        System.out.println("After Removing Element:");
+        for (int i=0; i<result; i++)
+        {
+            System.out.print(nums[result]+" ");
+        }
     }
 }
