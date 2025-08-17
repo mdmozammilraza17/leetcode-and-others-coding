d637cd1162e90d41b97f6f2f45b988aba3571716
mdmozammilraza17 <mdmozammilraza06@gmail.com>
2025-08-17 15:45:46 +0530

    Remove duplicates from sorted array

--- Remove Duplicates from Sorted Array -  Leetcode-26/src/Main.java
+++ Remove Duplicates from Sorted Array -  Leetcode-26/src/Main.java
@@ -1,5 +1,29 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i=1; i<nums.length; i++)
+        {
+            if (nums[i] != nums[i-1])
+            {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int [] nums = {0,0,1,1,1,2,2,3,3,4};
+        Solution solution = new Solution();
+        int result = solution.removeDuplicates(nums);
+
+        System.out.println("After Removing Duplicates:");
+
+        // Iterate the duplicate from sorted array based on j result.
+        for (int i=0; i<result; i++)
+        {
+            System.out.print(nums[i]+" ");
+        }
     }
 }
